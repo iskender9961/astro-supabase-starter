83019aa70962f284f3a418bf461d72400e3fe1db
Omer Burak Iskender <omerburakiskender@gmail.com>
2026-02-24 06:32:55 +0300

    Revert "Launch new website experience and content"
    This reverts commit 4fe419c424a2f03aadeb9278f0437f5a2ac0afbf.

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,16 +8,4 @@ const guides = defineCollection({
     })
 });
 
-const blog = defineCollection({
-    loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        date: z.string(),
-        author: z.string().optional(),
-        image: z.string().optional(),
-        tags: z.array(z.string()).optional(),
-    })
-});
-
-export const collections = { guides, blog };
+export const collections = { guides };
